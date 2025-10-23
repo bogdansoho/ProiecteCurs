a66644598361a1b4e57c278f4034f9b93ea5d2f1
bogdansoho <bogdansoho58@gmail.com>
2025-10-23 13:35:25 +0300

    Am comentat exercitiul

--- org/src/main/java/org/example/PrimaTema.java
+++ org/src/main/java/org/example/PrimaTema.java
@@ -2,12 +2,12 @@ package org.example;
 
 public class PrimaTema {
     public static void main(String[] args) {
- String primaLinie = "Salut ! ";
- String aDouaLinie = "Numele meu este ";
- String aTreiaLinie = "Bogdan .";
-
- System.out.println(primaLinie + aDouaLinie + aTreiaLinie);
-
+// String primaLinie = "Salut ! ";
+// String aDouaLinie = "Numele meu este ";
+// String aTreiaLinie = "Bogdan .";
+//
+// System.out.println(primaLinie + aDouaLinie + aTreiaLinie);
+//
 
     }
 }
