197a1007c601e27303aed43d11abdea6d27325f1
bogdansoho <bogdansoho58@gmail.com>
2025-10-23 13:41:24 +0300

    Am adaugat 2 variabile de 10 caractere si am realizat adunare, scadere, inmultire si impartire.

--- org/src/main/java/org/example/PrimaTema.java
+++ org/src/main/java/org/example/PrimaTema.java
@@ -8,7 +8,22 @@ public class PrimaTema {
 //
 // System.out.println(primaLinie + aDouaLinie + aTreiaLinie);
 //
-  final double dataNastere = 27.1941;
-  System.out.println(dataNastere);
+        final double dataNastere = 27.1941;
+        System.out.println(dataNastere);
+
+        long a = 10987654321L;
+        long b = 89076543214L;
+
+        long adunare = a + b;
+        long scadere = a - b;
+        long inmultire = a * b;
+        long impartire = a / b;
+
+        System.out.println(a + " + " + b + " = " + adunare);
+        System.out.println(a + " - " + b + " = " + scadere);
+        System.out.println(a + " * " + b + " = " + inmultire);
+        System.out.println(a + " / " + b + " = " + impartire);
+
+
     }
 }
