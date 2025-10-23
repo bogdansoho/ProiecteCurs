20cb5394e2d811f3457cd23a66eaeb497f7ec5dc
bogdansoho <bogdansoho58@gmail.com>
2025-10-23 13:34:37 +0300

    Am creat 3 variablile de tip string si le-am printat

--- org/src/main/java/org/example/PrimaTema.java
+++ org/src/main/java/org/example/PrimaTema.java
@@ -2,7 +2,11 @@ package org.example;
 
 public class PrimaTema {
     public static void main(String[] args) {
+ String primaLinie = "Salut ! ";
+ String aDouaLinie = "Numele meu este ";
+ String aTreiaLinie = "Bogdan .";
 
+ System.out.println(primaLinie + aDouaLinie + aTreiaLinie);
 
 
     }
