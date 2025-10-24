70d50011917641422b082ef1512a114bea26a9d0
bogdansoho <bogdansoho58@gmail.com>
2025-10-24 11:26:54 +0300

    Am importat clasa "Scanner" ca userul sa poata introduce anul dorit

--- org/src/main/java/org/example/Calendar.java
+++ org/src/main/java/org/example/Calendar.java
@@ -1,5 +1,14 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Calendar {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduceti un an intre 1900 si 2016 : ");
+
+        int year = scanner.nextInt();
+
+    }
 }
