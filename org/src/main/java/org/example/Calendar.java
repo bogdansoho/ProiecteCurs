56fa07626f1793f7e7be23e27b970b90755dd3f4
bogdansoho <bogdansoho58@gmail.com>
2025-10-24 11:49:33 +0300

    Am determinat daca anul are o zi in plus sau nu , si am printat rezultatul.

--- org/src/main/java/org/example/Calendar.java
+++ org/src/main/java/org/example/Calendar.java
@@ -10,5 +10,13 @@ public class Calendar {
 
         int year = scanner.nextInt();
 
+        int zi = 28;
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            zi = 29;
+        }
+
+        System.out.println("Numarul de zile din februarie pentru anul " + year + " este de " + zi + " zile.");
+
     }
 }
