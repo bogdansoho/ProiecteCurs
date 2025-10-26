faf7ef3add27d5625648ab57de469804e814a42b
bogdansoho <bogdansoho58@gmail.com>
2025-10-26 12:53:32 +0200

    Am comentat exercitiul

--- org/src/main/java/org/example/Rectangle.java
+++ org/src/main/java/org/example/Rectangle.java
@@ -13,23 +13,25 @@ public class Rectangle {
 //            System.out.println("*");
 //        }
 
-        int width = 10;
-        int height = 5;
-        for (int i = 0; i < height; i++) {
-            if (i == 0 || i == height - 1) {
-                for (int j = 0; j < width; j++) {
-                    System.out.print("*");
-                }
-            } else {
+//        int width = 10;
+//        int height = 5;
+//        for (int i = 0; i < height; i++) {
+//            if (i == 0 || i == height - 1) {
+//                for (int j = 0; j < width; j++) {
+//                    System.out.print("*");
+//                }
+//            } else {
+//
+//                System.out.print("*");
+//                for (int j = 1; j < width - 1; j++) {
+//                    System.out.print(" ");
+//                }
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//
+//        }
 
-                System.out.print("*");
-                for (int j = 1; j < width - 1; j++) {
-                    System.out.print(" ");
-                }
-                System.out.print("*");
-            }
-            System.out.println();
 
-        }
     }
 }
