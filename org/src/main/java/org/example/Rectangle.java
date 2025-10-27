aeef32af035cfc5bb9a16ff15608f18478751a47
bogdansoho <bogdansoho58@gmail.com>
2025-10-27 11:47:42 +0200

    Am adaugat si ultimul cub din tema

--- org/src/main/java/org/example/Rectangle.java
+++ org/src/main/java/org/example/Rectangle.java
@@ -31,7 +31,24 @@ public class Rectangle {
 //            System.out.println();
 //
 //        }
+        int height = 6;
+        int width = 8;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
 
-
+                if ((i == 0 && j == 0) ||
+                        (i == 0 && j == width - 1) ||
+                        (i == height - 1 && j == 0) ||
+                        (i == height - 1 && j == width - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
+
+
+
