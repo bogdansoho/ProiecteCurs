d4b0d6ef647df40af2792ce2ece48d219ee4c173
bogdansoho <bogdansoho58@gmail.com>
2025-10-29 09:43:23 +0200

    Am adaugat getters

--- org/src/main/java/org/example/booksale/Author.java
+++ org/src/main/java/org/example/booksale/Author.java
@@ -4,12 +4,21 @@ public class Author {
     public String name;
     public String email;
 
-    public Author(){
+    public Author() {
 
     }
-    public Author(String name, String email){
+
+    public Author(String name, String email) {
         this.name = name;
         this.email = email;
-
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
 }
