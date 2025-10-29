9e3480af14d50618482123ccb7e5606c41d5403c
bogdansoho <bogdansoho58@gmail.com>
2025-10-29 10:03:42 +0200

    Am facut acelasi lucru cu clasa "book" si am printat rezultatul final in clasa main "library"

--- org/src/main/java/org/example/booksale/Book.java
+++ org/src/main/java/org/example/booksale/Book.java
@@ -1,4 +1,36 @@
 package org.example.booksale;
 
 public class Book {
+    public static String name;
+    public static int year;
+    public static String Author;
+    public static double price;
+
+    public Book() {
+
+    }
+
+    public Book(String name, double price , String Author, int year) {
+        this.name = name;
+        this.year = year;
+        this.Author = Author;
+        this.price = price;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static int getYear() {
+        return year;
+    }
+
+    public static String getAuthor() {
+        return Author;
+    }
+
+    public static double getPrice() {
+        return price;
+    }
+
 }
--- org/src/main/java/org/example/booksale/Library.java
+++ org/src/main/java/org/example/booksale/Library.java
@@ -2,5 +2,9 @@ package org.example.booksale;
 
 public class Library {
     public static void main(String[] args) {
+        Book book = new Book("Cartea Junglei", 59.99, "Soho Bogdan", 1990);
+
+        System.out.println("Cartea <" + Book.getName() + "> este la pretul de " + Book.getPrice() + " si este scrisa de " + Book.getAuthor() + " in anul " + Book.getYear());
+
     }
 }
