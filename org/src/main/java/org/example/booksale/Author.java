130e0f6d7d09898e15bc55b4374b869e76fe5266
bogdansoho <bogdansoho58@gmail.com>
2025-10-29 09:36:51 +0200

    Am adaugat 2 stringuri publice , am creat un contructor default si unul cu parametrii

--- org/src/main/java/org/example/booksale/Author.java
+++ org/src/main/java/org/example/booksale/Author.java
@@ -1,5 +1,15 @@
 package org.example.booksale;
 
 public class Author {
+    public String name;
+    public String email;
 
+    public Author(){
+
+    }
+    public Author(String name, String email){
+        this.name = name;
+        this.email = email;
+
+    }
 }
