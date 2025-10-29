2107f05b856dd4539839dd9af0f5dd0441a3bd33
bogdansoho <bogdansoho58@gmail.com>
2025-10-29 09:30:20 +0200

    Am creat un nou pachet numit "booksale" si am adaugat 3 clase

---
+++ org/src/main/java/org/example/booksale/Book.java
@@ -0,0 +1,4 @@
+package org.example.booksale;
+
+public class Book {
+}
---
+++ org/src/main/java/org/example/booksale/Library.java
@@ -0,0 +1,6 @@
+package org.example.booksale;
+
+public class Library {
+    public static void main(String[] args) {
+    }
+}
