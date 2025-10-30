b88a5de1c0a5f4bda0945f862b0a6db329f4c48b
bogdansoho <bogdansoho58@gmail.com>
2025-10-30 10:43:17 +0200

    Am adaugat constructorul

--- org/src/main/java/org/example/light/Light.java
+++ org/src/main/java/org/example/light/Light.java
@@ -2,6 +2,14 @@ package org.example.light;
 
 public class Light {
 
+    private boolean isOn;
+    private int brightness = 0;
+
+    public Light() {
+        this.isOn = true;
+        this.brightness = 0;
+
+    }
 
 
 }
