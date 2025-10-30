e54f4c5772b9cc66ccfa015f110947adee756a3d
bogdansoho <bogdansoho58@gmail.com>
2025-10-30 11:19:31 +0200

    Am adaugat parametrii si creat if statements

--- org/src/main/java/org/example/light/Light.java
+++ org/src/main/java/org/example/light/Light.java
@@ -6,10 +6,44 @@ public class Light {
     private int brightness = 0;
 
     public Light() {
-        this.isOn = true;
+        this.isOn = false;
         this.brightness = 0;
 
     }
 
+    public void turnOn() {
+        if (!isOn) {
+            isOn = true;
+            System.out.println("The light is turned on ");
+        }
+    }
 
+    public void turnOff() {
+        if (isOn) {
+            isOn = false;
+            System.out.println("The light is turned off ");
+        }
+    }
+
+    public void dim() {
+        if (isOn && brightness > 50) {
+            brightness = 50;
+            System.out.println("The light is now dimmed with a brightness of : " + brightness);
+        }
+    }
+    public void brighten() {
+        if (isOn && brightness < 100) {
+            brightness = 100;
+            System.out.println("The light is now brighten with a brightness of : " + brightness);
+        }
+    }
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public int getBrightness() {
+        return brightness;
+    }
 }
+
+
